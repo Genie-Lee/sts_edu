a936a1ef1a2cb991058177ed82270d7b78cab792
Genie-Lee <genie141930@gmail.com>
2019-12-15 18:46:28 +0900

    Spring_start

---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+
+}
