a64bca9e57db490deb3e52f59b50ae805bd881a9
Genie-Lee <genie141930@gmail.com>
2019-12-18 13:33:18 +0900

    12-18_13:33

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,10 +1,26 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
 
 @Controller
-@RequestMapping("/sample/")
+@RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+@Log4j
 public class SampleController {
 
+	@RequestMapping("")
+	public void basicGet() {
+		
+		log.info("basic get.............");
+		
+	}
+	
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+		log.info("basic get only get.............");
+	}
 }
