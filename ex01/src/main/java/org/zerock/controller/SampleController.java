19dc21ef108d1ae63adbd1e33aa9951ee1b75e42
Genie-Lee <genie141930@gmail.com>
2019-12-20 18:01:34 +0900

    12-20_18:01

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -98,5 +103,44 @@ public class SampleController {
 		log.info("todo: " + todo);
 		
 		return "ex03";
+	}
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+		
+		return "/sample/ex04";
+	}
+	
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05.......");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		
+		log.info("/ex06...........");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		
+		log.info("/ex07.........");
+		
+		//{"name": "홍길동"}
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
 	}
 }
