bfabfc5a2d06ec28b59dc7e8b3d0bfa9c5548a3e
Genie-Lee <genie141930@gmail.com>
2019-12-26 18:02:06 +0900

    12-26_18::02

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,43 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		
+		model.addAttribute("list", service.getList());
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+}
